7f10936b0d3bc0fe6ae7bea6e11116af5a39a26f
Lyrid <support@lyrid.co.id>
2020-04-21 11:15:12 +0700

    Initial Commit

---
+++ Smart APP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smart APP
+//
+//  Created by Lyrid on 4/21/20.
+//  Copyright © 2020 Lyrid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
